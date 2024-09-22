8ce04bcf8baea0f1a34e3e95be9eba39c2a210ed
HumphyO <humphreymwebi18@gmail.com>
2024-09-21 23:36:37 -0700

    deleted match_case_calculator.py

--- control-flow/match_case_calculator.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-input("Choose the operation (+, -, *, /): ")
-
-match case operation:
-    case "addition"
